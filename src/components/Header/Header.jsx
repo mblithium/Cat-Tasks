43e4d0bac2f18c69c225a3ae909d1401883a345b
mblithium <matheusmcy1@gmail.com>
2022-11-07 22:28:37 -0300

    Add assets directory with images

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import style from '../Styles/Header.module.css'
+
+function Header() {
+    return (
+        <header className={style.headermain}>
+            <img src='src/assets/Header.svg'></img>
+        </header>
+    )
+}
+
+export default Header;
