df02f0e6a41edeef27feda96025cf4045c2237c7
mblithium <matheusmcy1@gmail.com>
2022-11-07 22:27:10 -0300

    Add a header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   return (
-    <div className="App">😺</div>
+    <div className="App">
+      <Header />
+    </div>
   )
 }
 
-export default App
+export default App;
